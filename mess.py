90741e2f908a9bb40a81788b33fff774a7d7d633
StarlordHarsh <hj101998@gmail.com>
2018-12-27 21:43:55 +0530

    update

---
+++ mess.py
@@ -0,0 +1,14 @@
+# Message Analyzer
+# Demonstrates the len() function and the in operator
+
+message = input("Enter a message: ")
+
+print("\nThe length of your message is:", len(message))
+
+print("\nThe most common letter in the English language, 'e',", end=" ")
+if "e" in message:
+    print("is in your message.")
+elif not "e" in message:
+    print("is not in your message.")
+
+input("\n\nPress the enter key to exit.")
