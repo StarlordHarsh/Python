b5e475c38aa6ac4d12bdc5564eaa6eae54bc27e4
Harsh Jaiswal <hj101998@gmail.com>
2019-01-29 23:17:45 +0530

    Update webs.py

--- webs.py
+++ webs.py
@@ -3,6 +3,15 @@ import requests
 
 search = input("Enter search item")
 params = {"q":search}
-r = requests.get("https://www.bing.com/search", params=params)
-soup = BeautifulSoup(r.text)
-print(soup.prettify())
+r = requests.get("http://www.bing.com/search", params=params)
+soup = BeautifulSoup(r.text, "html.parser")
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("Li", {"class": "b_algo"})
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
